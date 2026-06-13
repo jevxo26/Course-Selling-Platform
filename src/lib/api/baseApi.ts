70b5ba7e3ba3b28d0b75e90640b915d83053f5c7
innowavesolution <innowavesolutioninfo@gmail.com>
2026-06-14 00:33:56 +0600

    update

--- src/lib/api/baseApi.ts
+++ src/lib/api/baseApi.ts
@@ -24,7 +24,7 @@ export const baseApi = createApi({
   reducerPath: "baseApi",
 
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://api.maruftech.online",
+    baseUrl: "https://api.maruftech.online",
 
     prepareHeaders: (headers, { getState }) => {
       const state = getState() as RootState;
